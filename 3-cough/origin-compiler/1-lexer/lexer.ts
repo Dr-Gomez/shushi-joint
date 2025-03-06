dc2bc6ae9ee26258ee22a7b48ffb3f64ebf21b9d
Marco A. Gomez <gomez.contact.mgz@gmail.com>
2025-03-06 22:06:07 +0000

    recreates lexer methodology to simplify process

--- 3-cough/origin-compiler/1-lexer/lexer.ts
+++ 3-cough/origin-compiler/1-lexer/lexer.ts
@@ -1,101 +1,47 @@
-import { isDigit } from "./detection.ts";
+import { isAlpha, isArithmeticOperator, isAssignmentOperator, isBinaryOperator, isBoolKeyword, isDigit, isIoKeyword, isLogicalKeyword, isLogicalOperator, isSelectionKeyword, isSequencingKeyword, isUnaryOperator } from "./detection";
 
-export enum TokenType {
-    SOF,
-    EOF,
-    IDENTIFIER,
-    TYPE,
-    INT_NUM,
-    REAL_NUM,
-    STRING,
-    BOOL,
-    KEYWORD,
-    ASSIGN_OPERATOR,
-    BIN_OPERATOR,
-    UNA_OPERATOR,
-    PUNCTUATOR,
-    TERMINATOR,
-    LEFT_ENCAPSULATOR,
-    RIGHT_ENCAPSULATOR,
-    COMMENT,
-    ERROR
-}
-
-export interface Token {
-    type: TokenType;
+interface Token {
     value: string;
+    type: string;
 }
 
-export interface TokenWrapper {
-    payload: Token | null;
-    index: number;
-}
-  
-function skipWhitespace(source: string, index: number): number {
-    while (/\s/.test(source[index])) {
-      index++;
-    }
-  
-    return index;
-}
+function tokenize(input: string): Array<Token> {
+    const tokensIn: Array<string> = input.split(/\s+/);
+    const tokensOut: Array<Token> = [];
 
-function handleNumber(source: string, index: number): TokenWrapper {
-    if (isDigit(source[index])) {
-        const startIndex = index;
-        index++;
-
-        let real: boolean = false
-
-        while (isDigit(source[index])) {
-            index++;
-        }
-
-        if (source[index] == ".") {
-            real = true;
-            while (isDigit(source[index])) {
-                index++;
-            }
-        }
-
-        const numberStr: string = source.slice(startIndex, index);
-        let type: TokenType.REAL_NUM | TokenType.INT_NUM;
+    for (const token of tokensIn) {
+        let tokenType = "";
         
-        if (real == true) {
-            type = TokenType.REAL_NUM
+        if (isAlpha(token[0])) {
+            tokenType = "ALPHA"
+        } else if (isDigit(token[0])) {
+            tokenType = "NUMBER"
+        } else if (isSelectionKeyword(token)) {
+            tokenType = "SELECTION_KEYWORD"
+        } else if (isSequencingKeyword(token)) {
+            tokenType = "SEQUENCING_KEYWORD"
+        } else if (isAssignmentOperator(token)) {
+            tokenType = "ASSIGNMENT_OPERATOR"
+        } else if (isArithmeticOperator(token)) {
+            tokenType = "ARITHMETIC_OPERATOR"
+        } else if (isBinaryOperator(token)) {
+            tokenType = "BINARY_OPERATOR"
+        } else if (isUnaryOperator(token)) {
+            tokenType = "UNARY_OPERATOR"
+        } else if (isLogicalKeyword(token)) {
+            tokenType = "LOGICAL_KEYWORD"
+        } else if (isLogicalOperator(token)) {
+            tokenType = "LOGICAL_OPERATOR"
+        } else if (isBoolKeyword(token)) {
+            tokenType = "BOOL_KEYWORD"
+        } else if (isIoKeyword(token)) {
+            tokenType = "IO_KEYWORD"
         } else {
-            type = TokenType.INT_NUM
+            tokenType = "ERROR"
         }
-
-        const numberToken: Token = {type: type, value: numberStr}
         
-        const wrapper: TokenWrapper = {payload: numberToken, index: index}
-        return wrapper
+        tokensOut.push({type: tokenType, value: token})
     }
 
-    const wrapper: TokenWrapper = {payload: null, index: index}
-    return wrapper
-}
-
-const instrQueue: Array<Function> = [
-
-]
-
-function makeToken(source: string) {
-    let instrIndex = 0
-    for (instrIndex; instrIndex < instrQueue.length; instrIndex++) {
-
-    }
-} 
-
-export function generateTokens(source: string) {
-    let tokenQueue: Array<Token> = []
-
-    let currentTokenWrapper: TokenWrapper = {
-        payload: {type: TokenType.SOF, value: ""},
-        index: 0,
-    }
-
-    while (currentTokenWrapper.payload?.type !== TokenType.EOF) {
-        
-    }
+    return tokensOut
 }
